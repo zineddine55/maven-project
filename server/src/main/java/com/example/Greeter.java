36361c88b67954652d192dc022b889d723fb166d
babsmbaye18 <40393271+babsmbaye18@users.noreply.github.com>
2019-01-28 16:36:19 +0100

    Modification pour tester la scrutation sur Jenkins

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,6 +9,6 @@ public class Greeter {
   }
 
   public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
+    return String.format("Hello babacar, %s!", someone);
   }
 }
